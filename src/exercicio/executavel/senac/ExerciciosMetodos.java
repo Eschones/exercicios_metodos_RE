c98fab058a737e5e8fb3b78215074c7c6cca97d6
Raphael Esch <raphael.esch@hotmail.com>
2025-10-07 13:33:10 -0300

    Exercicio 1

--- src/exercicio/executavel/senac/ExerciciosMetodos.java
+++ src/exercicio/executavel/senac/ExerciciosMetodos.java
@@ -1,5 +1,8 @@
 package exercicio.executavel.senac;
 
+import exercicio1.pacote.senac.SomaDoisNumeros;
+import java.util.Scanner;
+
 /**
  *
  * @author Raphael Esch
@@ -8,8 +11,24 @@ public class ExerciciosMetodos {
 
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("---Soma de dois numeros---");
+        System.out.println("Informe o primeiro numero: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.println("\nInforme o segundo numero: ");
+        int num2 = scanner.nextInt();
+        
+        SomaDoisNumeros calcular = new SomaDoisNumeros();
+        
+        int resultado = calcular.somar(num1, num2);
+        
+        System.out.println("---O resultado final---");
+        System.out.println("A soma dos dois numeros deu: " + resultado);
         
         
     }
+    
     
 }
--- src/exercicio1/pacote/senac/somaDoisNumeros.java
+++ src/exercicio1/pacote/senac/somaDoisNumeros.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exercicio1.pacote.senac;
 
 /**
  *
  * @author Raphael Esch
  */
-public class somaDoisNumeros {
+public class SomaDoisNumeros {
+    
+    public int somar (int num1, int num2){
+    
+        return num1 + num2;
+        
+    }
     
 }
