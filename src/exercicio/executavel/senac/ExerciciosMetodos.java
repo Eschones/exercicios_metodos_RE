146c568895e65fa5f5f82ee231ea1c09b4d49e33
Raphael Esch <raphael.esch@hotmail.com>
2025-10-07 13:16:13 -0300

    Primeiro commit c gitignore

---
+++ src/exercicio/executavel/senac/ExerciciosMetodos.java
@@ -0,0 +1,15 @@
+package exercicio.executavel.senac;
+
+/**
+ *
+ * @author Raphael Esch
+ */
+public class ExerciciosMetodos {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
